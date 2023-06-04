587a61a41c062455f981b92e1fdd48b28fd66dac
Amin Mustafa Boankod <amin.m.boankod@gmail.com>
2023-06-04 23:10:03 +0200

    feat(testAddDelete)

--- farmer/src/main/java/edu/craptocraft/ResourceFruit.java
+++ farmer/src/main/java/edu/craptocraft/ResourceFruit.java
@@ -9,8 +9,12 @@ import jakarta.ws.rs.core.MediaType;
 import java.util.Set;
 import edu.craptocraft.entity.Fruit;
 import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 
 
@@ -37,6 +41,26 @@ public class ResourceFruit {
     @Produces(MediaType.APPLICATION_JSON)
     public Set<Fruit> list(){
         return service.list();
+    }
+
+    @POST
+    @Transactional
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Set<Fruit> add(@Valid Fruit fruit){
+        service.add(fruit);
+        return this.list();
+    }
+
+
+
+    @DELETE
+    @Transactional
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Set<Fruit> remove(@Valid Fruit fruit){
+        service.remove(fruit.getName());
+        return this.list();
     }
 
 }

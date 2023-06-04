e54882aa9d4a0eeff113f74ce79c255fcc03994a
Amin Mustafa Boankod <amin.m.boankod@gmail.com>
2023-06-04 16:06:30 +0200

    feat(entities, onetomany and manytoone)

---
+++ farmer/src/main/java/edu/craptocraft/entity/Farmer.java
@@ -0,0 +1,59 @@
+package edu.craptocraft.entity;
+
+import java.util.Set;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "farmer")
+public class Farmer extends PanacheEntityBase {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "location")
+    private String location;
+
+    @OneToMany(mappedBy = "farmer")
+    public Set<Fruit> fruits;
+
+    public Farmer() {
+    }
+
+    // getter y setter
+
+    public Long getId() {
+        return id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+
+
+}
---
+++ farmer/src/main/java/edu/craptocraft/entity/Fruit.java
@@ -0,0 +1,62 @@
+package edu.craptocraft.entity;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "fruit")
+public class Fruit extends PanacheEntityBase
+{
+    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name="description")
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "farmer_id")
+    public Farmer farmer;
+    
+    public Fruit() {
+    }
+
+
+    // getter y setter
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setName( String name ) {
+        this.name = name;
+    }
+
+    public void setDescription( String description ) {
+        this.description = description;
+    }
+
+
+    
+
+}
